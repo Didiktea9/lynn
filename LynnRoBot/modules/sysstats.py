c2f24f4654d309f5aec90a0eed0e234661f53211
didikteatochhawng <81432899+didikteatochhawng@users.noreply.github.com>
2021-11-02 00:13:59 +0530

    Create sysstats.py

---
+++ LynnRoBot/modules/sysstats.py
@@ -0,0 +1,28 @@
+import asyncio
+
+import os
+
+import subprocess
+
+import time
+
+import psutil
+from LynnRoBot.utils import formatter
+from LynnRoBot import StartTime
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - StartTime)
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    process = psutil.Process(os.getpid())
+    stats = f"""
+Lynn@DikaMs_bot
+------------------
+UPTIME: {formatter.get_readable_time((bot_uptime))}
+BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
+CPU: {cpu}%
+RAM: {mem}%
+DISK: {disk}%
+"""
+    return stats
