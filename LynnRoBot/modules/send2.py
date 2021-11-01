1a948f3b480ee557a7fcd7be07ac0fda2cb81175
didikteatochhawng <81432899+didikteatochhawng@users.noreply.github.com>
2021-11-02 00:11:22 +0530

    Create send2.py

---
+++ LynnRoBot/modules/send2.py
@@ -0,0 +1,17 @@
+from LynnRoBot import pbot as app
+
+from LynnRoBot.modules.sysstats import bot_sys_stats
+
+from pyrogram import filters
+
+@app.on_callback_query(filters.regex("stats_callback"))
+
+async def stats_callbacc(_, CallbackQuery):
+
+    text = await bot_sys_stats()
+
+    await app.answer_callback_query(
+
+        CallbackQuery.id, text, show_alert=True
+
+    )
